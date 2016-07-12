d6bc5716a20d3961eed4fee70ccdd2c97571f338
jianiyang <376692150@163.com>
2016-07-12 14:32:20 -0700

    decode and encode both

--- src/StringEncode.java
+++ src/StringEncode.java
@@ -35,7 +35,7 @@ public class StringEncode {
         for(String out : result){
             System.out.println(out);
         }
-        String result2 = res.stringEncode(result);
-        System.out.println(result2);
+        //String result2 = res.stringEncode(result);
+       // System.out.println(result2);
     }
 }
